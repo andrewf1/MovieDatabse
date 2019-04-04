cc334ee3df42f607d625a2701f3626835f27febc
Unknown <ashtonrodriquez@hotmail.com>
2019-04-03 21:31:19 -0400

    Created home page

--- movie_db/src/views/Home.vue
+++ movie_db/src/views/Home.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div justify-center class="banner">
+        <h1 class="head"> Movies For Everyone.</h1>
+        <h2 class="second"> Millions of movies at your fingertips </h2>
+         <b-button href="/about">Sign Up</b-button>
+    </div>
+    <b-container fluid class="movies">
+      <h1 class="subheader"> NEW ARRIVALS </h1>
+
+      <b-row>
+        <b-col>
+          <b-img src="http://www.gstatic.com/tv/thumb/v22vodart/14060049/p14060049_v_v8_ad.jpg" class="image"></b-img>
+          <p class="small"> Captain Marvel </p>
+        </b-col>
+
+        <b-col>
+          <b-img src="https://m.media-amazon.com/images/M/MV5BYTExYTc3YTMtMDY2YS00YTFhLTgwODEtNWQ2MmMzOGZhNTU5XkEyXkFqcGdeQXVyNDExODY2MjU@._V1_.jpg" class="image"></b-img>
+          <p class="small"> A Madea Family Funeral </p>
+        </b-col>
+
+        <b-col>
+          <b-img src="http://allears.net/wp-content/uploads/2019/01/Dumbo-Live-Movie-Poster.jpg" class="image"></b-img>
+          <p class="small"> Dumbo </p>
+        </b-col>
+
+        <b-col>
+          <b-img src="https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_.jpg" class="image"></b-img>
+          <p class="small"> Us </p>
+        </b-col>
+
+        <b-col>
+          <b-img src="https://www.joblo.com/assets/images/joblo/posters/2018/10/The-Upside-poster-1_thumb.jpg" class="image"></b-img>
+          <p class="small"> The Upside </p>
+        </b-col>
+    
+      </b-row>
+   
+      
+
+    </b-container>
+    <div class="login">
+
+    </div>
+  
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.banner{
+  min-height: 70vh;
+  background-size: cover;
+  background-image: url("https://images.unsplash.com/6/blurred_lines.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80");
+  padding-top: 20vh;
 }
-</script>
+.head{
+  color: white;
+  font-size: 10vh;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: 400;
+}
+
+.second{
+  color: white;
+  margin-bottom: 5vh;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+}
+
+.movies{
+  background-color: lightcoral;
+  min-height: 80vh;
+  padding: 7vh;
+}
+
+.subheader{
+  font-size: 8vh;
+  font-family: 'Courier New', Courier, monospace;
+  color: white;
+  padding-bottom: 5vh;
+}
+
+.login{
+  background-color: aliceblue;
+  min-height: 50vh;
+}
+
+.image{
+width: 180px;
+height: 275px;
+}
+
+.small{
+  font-size: 25px;
+  font-weight: bold;
+  font-family: 'Courier New', Courier, monospace;
+  padding-top: 1.5vh;
+  color: white;
+}
+
+
+</style>
+
