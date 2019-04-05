4d1da0ab67fe411b38c4fcbc70ea00e9f0826d6d
Nadia Thomas <NadiThomas7@gmail.com>
2019-04-04 20:50:24 -0400

    Created insert statements for movie databas

---
+++ backend_psql/data/Insert_Statements_For_Movies_Database.sql
@@ -0,0 +1,268 @@
+
+
+/*Insert Statements For Movies in Database*/
+
+/*Comedy*/
+
+INSERT INTO movie (title, genre, year, stock)
+values ('A Madea Family Funeral', 'Comedy', 2019, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('22 Jump Street', 'Comedy', 2014, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Bridesmaids', 'Comedy', 2011, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Step Brothers', 'Comedy', 2008, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Dumb and Dumber', 'Comedy', 1994, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Campaign', 'Comedy', 2012, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Bruce Almighty', 'Comedy', 2003, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Ride Along', 'Comedy', 2014, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Rush Hour', 'Comedy', 1998, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('White Chicks', 'Comedy', 2004, 5);
+
+/*Horror*/
+
+INSERT INTO movie (title, genre, year, stock)
+values ('It', 'Horror', 2017, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Get Out', 'Horror', 2017, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Conjuring', 'Horror', 2013, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Insidious', 'Horror', 2010, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Paranormal Activity', 'Horror', 2019, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Boy', 'Horror', 2016, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Saw', 'Horror', 2004, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Halloween', 'Horror', 2018, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Hush', 'Horror', 2016, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Shining', 'Horror', 1980, 5);
+
+/*Romance*/
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Notebook', 'Romance', 2004, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Crazy Rich Asains', 'Romance', 2018, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Fault in Our Stars', 'Romance', 2014, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Twilight', 'Romance', 2008, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Dirty Dancing', 'Romance', 1987, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Titantic', 'Romance', 1997, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Maid in Manhattan', 'Romance', 2004, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Pretty in Pink', 'Romance', 1986, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('To All the Boys Ive Loved Before', 'Romance', 2018, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Great Gatsby', 'Romance', 2013, 5);
+
+/*Animation*/
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Shrek', 'Animation', 2001, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Incredibles', 'Animation', 2004, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Coco', 'Animation', 2017, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Moana', 'Animation', 2016, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Finding Nemo', 'Animation', 2003, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Despical Me', 'Animation', 2010, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Toy Story', 'Animation', 1995, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Mulan', 'Animation', 1998, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Dumbo', 'Animation', 2019, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Lion KIng', 'Animation', 1994, 5);
+
+/*Drama*/
+
+INSERT INTO movie (title, genre, year, stock)
+values ('A Star Is Born', 'Drama', 2018, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Help', 'Drama', 2011, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Blind Side', 'Drama', 2009, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Forrest Gump', 'Drama', 1994, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Pursuit of Happyness', 'Drama', 2006, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Cast Away', 'Drama', 2000, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Intouchables', 'Drama', 2011, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Life Of Pi', 'Drama', 2012, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Upside', 'Drama', 2019, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Creed', 'Drama', 2015, 5);
+
+/*Thriller*/
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Us', 'Thriller', 2019, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Gone Girl', 'Thriller', 2014, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Shutter Island', 'Thriller', 2010, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Inception', 'Thriller', 2010, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Taken', 'Thriller', 2008, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Sixth Sense', 'Thriller', 1999, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Jaws', 'Thriller', 1975, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Hide and Seek', 'Thriller', 2005, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Split', 'Thriller', 2016, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Now You See Me', 'Thriller', 2013, 5);
+
+/*Sci-Fi*/
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Guardians of the Galaxy', 'Sci-Fi', 2014, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Avatar', 'Sci-Fi', 2009, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Aliens', 'Sci-Fi', 1986, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Matrix', 'Sci-Fi', 1999, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Jurassic World', 'Sci-Fi', 2015, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('E.T. the Extra-Terrestrial', 'Sci-Fi', 1982, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Interstella', 'Sci-Fi', 2014, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Rise of the Planet of the Apes', 'Sci-Fi', 2011, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Arrival', 'Sci-Fi', 2016, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('District 9', 'Sci-Fi', 2009, 5);
+
+/*Action*/
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Wonder Woman', 'Action', 2017, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Black Panther', 'Action', 2017, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Mission Impossible-Ghost Protocol', 'Action', 2011, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Venom', 'Action', 2018, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Bourne Identity', 'Action', 2000, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Mad Max: Fury Road', 'Action', 2015, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('The Avengers', 'Action', 2012, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Captain Marvel', 'Action', 2019, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('Die Hard', 'Action', 1988, 5);
+
+INSERT INTO movie (title, genre, year, stock)
+values ('RoboCop', 'Action', 1987, 5);
+
+
+
+
+
+
+
+
+
