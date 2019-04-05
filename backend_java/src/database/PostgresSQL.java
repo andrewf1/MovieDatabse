369e95e3497e8179346b9715a183b60a0bb4b4ec
Andrew Fernandez <fdez26@ymail.com>
2019-04-04 21:36:57 -0400

    begin java connection to the db

--- backend_java/src/database/PostgresSQL.java
+++ backend_java/src/database/PostgresSQL.java
@@ -1,5 +1,29 @@
 package database;
 
-public class PostgresSQL {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+// referenced from postgrestutorial.com
+public class PostgresSQL {
+	// might have to add port number to url (5432) <database_host>:<port>
+	private final String url = "jdbc:postgresql://localhost:5432/movie_db";
+	private final String user = "postgres";
+	private final String password = "password";
+	
+	 public Connection connect() {
+	        Connection connection = null;
+	        try {
+	            connection = DriverManager.getConnection(url, user, password);
+	            System.out.println("Connected to the PostgreSQL server successfully.");
+	        } catch (SQLException ex) {
+	            System.out.println(ex.getMessage());
+	        }
+	 
+	        return connection;
+	    }
+   public static void main(String[] args) {
+	   PostgresSQL app = new PostgresSQL();
+	   app.connect();
+   }
 }
