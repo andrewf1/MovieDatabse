6f80794a960068d011c46c04fb3b9bc5ba67293c
Andrew Fernandez <fdez26@ymail.com>
2019-04-08 20:00:49 -0400

    updated insert_customer function to work properly for the way the customer table currently works

--- backend_psql/functions/insert_customer.sql
+++ backend_psql/functions/insert_customer.sql
@@ -2,6 +2,8 @@
 @brief: function that inserts a new customer given all it's needed information. Called during sign up
 usage: select insert_customer from insert_customer('Andrew', 'Fernandez', 'email@email.com', 
 '06/11/1997', '123 Main Street Tampa, FL 34683', 'passwoRD.123');
+
+returns 0 : successful, returns 1 : improper email, returns 2 : email already exists
 */
 CREATE OR REPLACE FUNCTION insert_customer(fname_i varchar, lname_i varchar, email_i varchar, dob_i
 	date, address_i varchar, pass_word_i varchar)
@@ -14,11 +16,14 @@ AS $BODY$
 DECLARE
 row_change integer;
 successful integer := 0;
-improper_password integer := 1;
-improper_email integer := 2;
-email_exists integer  := 3;
+improper_email integer := 1;
+email_exists integer  := 2;
 
 BEGIN
+	IF (SELECT COUNT(*) FROM customer where email = email_i) = 0 THEN
+		RETURN email_exists; -- checks if the email exists
+	END IF;
+
 	insert into customer (fname, lname, email, dob, address, pass_word)
 	values (fname_i, lname_i, email_i, dob_i, address_i, pass_word_i);
 	GET DIAGNOSTICS row_change = ROW_COUNT;
@@ -26,9 +31,7 @@ BEGIN
 	IF row_change = 1 THEN
 		return successful;
 	ELSE
-		return false;
+		return improper_email;
 	END IF; 
 END;
 $BODY$;
-
-CHANGE TO RETURN INT INSTEAD OF BOOLEAN
