fc87987452b68645804fb90d1a7989f7315de4f2
Unknown <ashtonrodriquez@hotmail.com>
2019-04-10 20:44:01 -0400

    Minor change

--- backend_express/movie_db/app.js
+++ backend_express/movie_db/app.js
@@ -1,7 +1,37 @@
 const express = require('express')
+const { Pool, Client } = require('pg')
 const routes = require('./routes/signup')
 const app = express()
 const port = 3000
+
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'movie_db',
+  password: 'spassword',
+  port: 5432,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'movie_db',
+  password: 'password',
+  port: 5432,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
 
 
 
