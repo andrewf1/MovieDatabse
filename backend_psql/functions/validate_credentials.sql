36b1331a16cc922e71fa6bdf795a7982dda8d856
Andrew Fernandez <fdez26@ymail.com>
2019-04-20 13:31:58 -0400

    add validate_credentials function

---
+++ backend_psql/functions/validate_credentials.sql
@@ -0,0 +1,28 @@
+/**
+@brief: function that takes an email and a password as input and ensures that the email exists, and
+if it does, ensures the password given is the one associated with the email. It returns true, but 
+returns false if it fails
+usage: 
+*/
+CREATE OR REPLACE FUNCTION validate_credentials(email_i varchar, password_i varchar)
+	RETURNS boolean
+	LANGUAGE 'plpgsql'
+	volatile
+	SECURITY DEFINER
+AS $BODY$
+
+DECLARE
+temprow RECORD;
+BEGIN
+	FOR temprow IN (select email, password from customers)
+	LOOP	
+		IF temprow.email = email_i THEN
+			IF temprow.password = password_i THEN
+				return true;
+			END IF;
+		END IF;
+	END LOOP;
+
+	return false;
+END;
+$BODY$;
