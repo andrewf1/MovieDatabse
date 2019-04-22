e640c7a5a1eb56fb5464c25a79e952613043e29d
Unknown <ashtonrodriquez@hotmail.com>
2019-04-22 16:46:15 -0400

    Updated movie catalog

--- movie_db/src/views/Catalog.vue
+++ movie_db/src/views/Catalog.vue
@@ -1,36 +1,36 @@
 <template>
     <div>
      <b-container fluid class="movies">
-        <h1 class="subheader"> Horror Movies </h1>
+        <h1 class="subheader"> Comedy Movies </h1>
 
         <b-row>
             <b-col>
-            <b-img src="http://www.gstatic.com/tv/thumb/v22vodart/14060049/p14060049_v_v8_ad.jpg" class="image"></b-img>
-            <p class="small"> Captain Marvel </p>
+            <b-img src="https://art-s.nflximg.net/8d056/253fc71de6915dea5125be2016b2e565cf28d056.jpg" class="image"></b-img>
+            <p class="small"> 22 Jump Street </p>
             <b-button v-on:click="count">Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://m.media-amazon.com/images/M/MV5BYTExYTc3YTMtMDY2YS00YTFhLTgwODEtNWQ2MmMzOGZhNTU5XkEyXkFqcGdeQXVyNDExODY2MjU@._V1_.jpg" class="image"></b-img>
-            <p class="small"> A Madea Family Funeral </p>
+            <b-img src="http://www.moviemars.com/GetProductImage.ashx?i=19452321.jpg&w=500&/19452321_500.jpg" class="image"></b-img>
+            <p class="small"> Ride Along </p>
             <b-button v-on:click="count">Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="http://allears.net/wp-content/uploads/2019/01/Dumbo-Live-Movie-Poster.jpg" class="image"></b-img>
-            <p class="small"> Dumbo </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/71tcRxPDnjL._SY606_.jpg" class="image"></b-img>
+            <p class="small"> Bridesmaids </p>
             <b-button v-on:click="count">Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_.jpg" class="image"></b-img>
-            <p class="small"> Us </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/51oUqGb18cL.jpg" class="image"></b-img>
+            <p class="small"> Stepbrothers </p>
             <b-button v-on:click="count">Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://www.joblo.com/assets/images/joblo/posters/2018/10/The-Upside-poster-1_thumb.jpg" class="image"></b-img>
-            <p class="small"> The Upside </p>
+            <b-img src="https://dtvimages.hs.llnwd.net/e1//db_photos/movies/AllPhotosAPGI/16298/16298_aa.jpg" class="image"></b-img>
+            <p class="small"> Dumb and Dumber </p>
             <b-button v-on:click="count">Add to Cart </b-button>
             </b-col>
         </b-row>
@@ -40,75 +40,75 @@
 
         <b-row>
             <b-col>
-            <b-img src="http://www.gstatic.com/tv/thumb/v22vodart/14060049/p14060049_v_v8_ad.jpg" class="image"></b-img>
-            <p class="small"> Captain Marvel </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/61bMkkwpduL._SY679_.jpg" class="image"></b-img>
+            <p class="small"> It </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://m.media-amazon.com/images/M/MV5BYTExYTc3YTMtMDY2YS00YTFhLTgwODEtNWQ2MmMzOGZhNTU5XkEyXkFqcGdeQXVyNDExODY2MjU@._V1_.jpg" class="image"></b-img>
-            <p class="small"> A Madea Family Funeral </p>
+            <b-img src="https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg" class="image"></b-img>
+            <p class="small"> Get Out </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="http://allears.net/wp-content/uploads/2019/01/Dumbo-Live-Movie-Poster.jpg" class="image"></b-img>
-            <p class="small"> Dumbo </p>
+            <b-img src="https://m.media-amazon.com/images/M/MV5BZjU5OWVlN2EtODNlYy00MjhhLWI0MDUtMTA3MmQ5MGMwYTZmXkEyXkFqcGdeQXVyNjE5MTM4MzY@._V1_.jpg" class="image"></b-img>
+            <p class="small"> The Conjuring </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_.jpg" class="image"></b-img>
-            <p class="small"> Us </p>
+            <b-img src="https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Insidious_poster.jpg/220px-Insidious_poster.jpg" class="image"></b-img>
+            <p class="small"> Insidious </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://www.joblo.com/assets/images/joblo/posters/2018/10/The-Upside-poster-1_thumb.jpg" class="image"></b-img>
-            <p class="small"> The Upside </p>
+            <b-img src="https://ae01.alicdn.com/kf/HTB11XhjAiCYBuNkSnaVq6AMsVXab/DIY-frame-Hush-Silence-Can-Be-Killer-Horror-Movie-Film-posters-and-print-home-decor-art.jpg_640x640.jpg" class="image"></b-img>
+            <p class="small"> Hush </p>
             <b-button>Add to Cart </b-button>
             </b-col>
         
         </b-row>
     </b-container>
          <b-container fluid class="movies">
-        <h1 class="subheader"> Horror Movies </h1>
+        <h1 class="subheader"> Romance Movies </h1>
 
         <b-row>
             <b-col>
-            <b-img src="http://www.gstatic.com/tv/thumb/v22vodart/14060049/p14060049_v_v8_ad.jpg" class="image"></b-img>
-            <p class="small"> Captain Marvel </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/51Td3Eetr5L.jpg" class="image"></b-img>
+            <p class="small"> The Notebook </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://m.media-amazon.com/images/M/MV5BYTExYTc3YTMtMDY2YS00YTFhLTgwODEtNWQ2MmMzOGZhNTU5XkEyXkFqcGdeQXVyNDExODY2MjU@._V1_.jpg" class="image"></b-img>
-            <p class="small"> A Madea Family Funeral </p>
+            <b-img src="http://static1.squarespace.com/static/51d35c6ee4b0564d574c83a7/53124d11e4b0d6e4133c223e/5a7e18f99140b748a4c9c686/1550799532388/?format=1500w" class="image"></b-img>
+            <p class="small"> Pretty in Pink </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="http://allears.net/wp-content/uploads/2019/01/Dumbo-Live-Movie-Poster.jpg" class="image"></b-img>
-            <p class="small"> Dumbo </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/51uJgSJ3TIL.jpg" class="image"></b-img>
+            <p class="small"> Twilight </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_.jpg" class="image"></b-img>
-            <p class="small"> Us </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/61PF6Oi%2BUhL._SY679_.jpg" class="image"></b-img>
+            <p class="small"> Dirty Dancing </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://www.joblo.com/assets/images/joblo/posters/2018/10/The-Upside-poster-1_thumb.jpg" class="image"></b-img>
-            <p class="small"> The Upside </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/51gEpO63aRL.jpg" class="image"></b-img>
+            <p class="small"> Titanic </p>
             <b-button>Add to Cart </b-button>
             </b-col>
         
         </b-row>
     </b-container>
          <b-container fluid class="movies">
-        <h1 class="subheader"> Horror Movies </h1>
+        <h1 class="subheader"> Action Movies </h1>
 
         <b-row>
             <b-col>
@@ -118,26 +118,26 @@
             </b-col>
 
             <b-col>
-            <b-img src="https://m.media-amazon.com/images/M/MV5BYTExYTc3YTMtMDY2YS00YTFhLTgwODEtNWQ2MmMzOGZhNTU5XkEyXkFqcGdeQXVyNDExODY2MjU@._V1_.jpg" class="image"></b-img>
-            <p class="small"> A Madea Family Funeral </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/A1wxXiiMuYL._SY679_.jpg" class="image"></b-img>
+            <p class="small"> Wonder Woman </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="http://allears.net/wp-content/uploads/2019/01/Dumbo-Live-Movie-Poster.jpg" class="image"></b-img>
-            <p class="small"> Dumbo </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/61apTYdaRXL.jpg" class="image"></b-img>
+            <p class="small"> Black Panther </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_.jpg" class="image"></b-img>
-            <p class="small"> Us </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/518jo3Xlf8L.jpg" class="image"></b-img>
+            <p class="small"> Venom </p>
             <b-button>Add to Cart </b-button>
             </b-col>
 
             <b-col>
-            <b-img src="https://www.joblo.com/assets/images/joblo/posters/2018/10/The-Upside-poster-1_thumb.jpg" class="image"></b-img>
-            <p class="small"> The Upside </p>
+            <b-img src="https://images-na.ssl-images-amazon.com/images/I/719SFBdxRtL._SY550_.jpg" class="image"></b-img>
+            <p class="small"> The Avengers </p>
             <b-button>Add to Cart </b-button>
             </b-col>
         
