0bfbe9dc56b8168b57a88d90d3161c0e84392688
Andrew Fernandez <fdez26@ymail.com>
2019-04-24 01:07:29 -0400

    fix syntax error

--- backend_psql/functions/update_customer.sql
+++ backend_psql/functions/update_customer.sql
@@ -12,19 +12,19 @@ BEGIN
 	IF type = 1 THEN
 		update customer set email = new_value where email = old_email;
 		GET DIAGNOSTICS row_change = ROW_COUNT;
-		return row_change
+		return row_change;
 	END IF;
 
 	IF type = 2 THEN
 		update customer set pass_word = new_value where email = old_email;
 		GET DIAGNOSTICS row_change = ROW_COUNT;
-		return row_change
+		return row_change;
 	END IF;
 
 	IF type = 3 THEN
 		update customer set address = new_value where email = old_email;
 		GET DIAGNOSTICS row_change = ROW_COUNT;
-		return row_change
+		return row_change;
 	END IF;
 
 	return 0;
