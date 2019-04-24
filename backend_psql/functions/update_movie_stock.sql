96affc5362f2cf80084635d95b1a08088dcf2fa7
Andrew Fernandez <fdez26@ymail.com>
2019-04-24 15:04:32 -0400

    add update_movie_stock function

---
+++ backend_psql/functions/update_movie_stock.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION update_movie_stock(mid_i integer)
+	RETURNS integer
+	LANGUAGE 'plpgsql'
+	volatile
+	SECURITY DEFINER
+AS $BODY$
+
+DECLARE
+new_stock integer;
+
+row_change integer;
+
+BEGIN
+	select stock into new_stock from movie where mid = mid_i;
+	new_stock := new_stock - 1;
+
+	update movie set stock = new_stock where mid = mid_i;
+	GET DIAGNOSTICS row_change = ROW_COUNT;
+
+	return row_change;
+END;
+$BODY$;
