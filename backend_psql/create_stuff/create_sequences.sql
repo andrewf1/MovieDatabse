6e042254e757672c263f1bbf2b4f36938a2c0eac
Andrew Fernandez <fdez26@ymail.com>
2019-04-23 21:20:19 -0400

    get rid of unnecessary uid sequence

--- backend_psql/create_stuff/create_sequences.sql
+++ backend_psql/create_stuff/create_sequences.sql
@@ -1,5 +1,3 @@
 -- this file contains the sql to create the sequences that will be used before creating the tables
-
-CREATE SEQUENCE uid INCREMENT BY 1 MINVALUE 1 NO MAXVALUE START WITH 1;
 CREATE SEQUENCE mid INCREMENT BY 1 MINVALUE 1 NO MAXVALUE START WITH 1;
 CREATE SEQUENCE transaction_num INCREMENT BY 1 MINVALUE 1 NO MAXVALUE START WITH 1;
