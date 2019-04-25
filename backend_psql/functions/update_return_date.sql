880a84f0f0718f985773837b222b4852874686d9
Andrew Fernandez <fdez26@ymail.com>
2019-04-24 22:05:04 -0400

    udpate function

--- backend_psql/functions/update_return_date.sql
+++ backend_psql/functions/update_return_date.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION update_return_date(email_i varchar, mid_i integer, return_date_i date)
+CREATE OR REPLACE FUNCTION update_return_date(transaction_num_i integer)
 	RETURNS integer
 	LANGUAGE 'plpgsql'
 	volatile
@@ -7,10 +7,10 @@ AS $BODY$
 
 DECLARE
 row_change integer;
-
+new_return_date date := now()::date;
 BEGIN
-	update purchase_history set return_date = return_date_i where email = email_i AND 
-		mid = mid_i;
+	update purchase_history set return_date = new_return_date where transaction_num = 
+		transaction_num_i;
 	GET DIAGNOSTICS row_change = ROW_COUNT;
 	return row_change;
 END;
