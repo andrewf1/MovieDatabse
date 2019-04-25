a1a3829ff73907a345c2e3b1f5052b512f392b52
Andrew Fernandez <fdez26@ymail.com>
2019-04-24 20:41:07 -0400

    change dates to be dates and not time_stamps

--- backend_psql/functions/get_checked_out_movies.sql
+++ backend_psql/functions/get_checked_out_movies.sql
@@ -6,7 +6,8 @@ CREATE OR REPLACE FUNCTION get_checked_out_movies(email_i varchar)
 AS $BODY$
 
 BEGIN
-	return query select P.mid, M.title, P.due_date, P.date_rented from movie M natural join 
+	return query select P.mid, M.title, P.due_date::date, P.date_rented::date from movie M 
+	natural join 
 	purchase_history P where email = email_i AND return_date IS NULL;
 END;
 $BODY$;
