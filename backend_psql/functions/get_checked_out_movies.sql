546afdbdc38c3bec3ff6bd009a4bf7c71d457a45
Andrew Fernandez <fdez26@ymail.com>
2019-04-24 21:46:54 -0400

    update the function to return the transaction_num

--- backend_psql/functions/get_checked_out_movies.sql
+++ backend_psql/functions/get_checked_out_movies.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE FUNCTION get_checked_out_movies(email_i varchar)
-	RETURNS table(mid_r integer, title_r varchar, due_date_r date, date_rented_r date)
+	RETURNS table(transactio_num_r integer, mid_r integer, title_r varchar, due_date_r date, 
+		date_rented_r date)
 	LANGUAGE 'plpgsql'
 	volatile
 	SECURITY DEFINER
 AS $BODY$
 
 BEGIN
-	return query select P.mid, M.title, P.due_date::date, P.date_rented::date from movie M 
-	natural join 
-	purchase_history P where email = email_i AND return_date IS NULL;
+	return query select P.transactio_num, P.mid, M.title, P.due_date::date, P.date_rented::date from 
+	movie M natural join purchase_history P where email = email_i AND return_date IS NULL;
 END;
 $BODY$;
